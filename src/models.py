efd01108db558525ea902c7b675aaa4ee4726a0d
gabyiy <gabyiy2000@hotmail.com>
2021-11-11 16:19:32 +0000

    next

--- src/models.py
+++ src/models.py
@@ -8,26 +8,53 @@ from eralchemy import render_er
 
 Base = declarative_base()
 
-class Person(Base):
-    __tablename__ = 'person'
-    # Here we define columns for the table person
-    # Notice that each column is also a normal Python instance attribute.
+class User(Base):
+    __tablename__ = 'user'
+   
+    id = Column(Integer, primary_key=True)
+    email = Column(String(250), nullable=False)
+    password = Column(String(250), nullable=False)
+    favorite = relationship("Favorite")
+
+class Favorite(Base):
+    __tablename__ = 'favorite'
+    
     id = Column(Integer, primary_key=True)
     name = Column(String(250), nullable=False)
+    user_id = Column(Integer, ForeignKey('user.id'))
+    character_id = Column(Integer, ForeignKey('character.id'))
+    planet_id = Column(Integer, ForeignKey('planet.id'))
+      
 
-class Address(Base):
-    __tablename__ = 'address'
-    # Here we define columns for the table address.
-    # Notice that each column is also a normal Python instance attribute.
+class Character(Base):
+    __tablename__='character'
+
     id = Column(Integer, primary_key=True)
-    street_name = Column(String(250))
-    street_number = Column(String(250))
-    post_code = Column(String(250), nullable=False)
-    person_id = Column(Integer, ForeignKey('person.id'))
-    person = relationship(Person)
+    name = Column(String(250), nullable=False)
+    height = Column(Integer, nullable=False)
+    mass = Column(Integer, nullable=False)
+    hair_color = Column(String(30))
+    skin_color = Column(String(30))
+    eye_color = Column(String(30))
+    birth_year = Column(String(250), nullable=False)
+    gender = Column(String(30), nullable=False)
+    favorite = relationship("Favorite")
 
-    def to_dict(self):
-        return {}
+class Planet(Base):
+    __tablename__='planet'
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String(250), nullable=False)
+    diameter = Column(Integer)
+    rotation_period = Column(Integer)
+    orbital_period = Column(Integer)
+    gravity = Column(String(250))
+    population = Column(Integer, nullable=False)
+    climate = Column(String(250))
+    terrain = Column(String(250), nullable=False)
+    surface_water = Column(Integer)  
+    favorite = relationship("Favorite")
+
 
 ## Draw from SQLAlchemy base
 render_er(Base, 'diagram.png')
